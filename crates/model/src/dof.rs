3da380beafc4fb2b05977f5baa7c58d700720ac7
Chetan Jangir <chetanrjj2002@gmail.com>
2026-05-20 23:02:26 +0530

    feat(model): Add dof model

---
+++ crates/model/src/dof.rs
@@ -0,0 +1,17 @@
+/// The global DOF index for node `i` (in the global stiffness matrix) with DOF `d` is:
+///   global_index = i * DOFS_PER_NODE + d as usize
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Dof {
+    /// Translation in X
+    Ux = 0,
+    /// Translation in Y
+    Uy = 1,
+    /// Rotation about Z
+    Rz = 2,
+}
+
+pub const DOFS_PER_NODE: usize = 3;
+
+pub fn global_dof_index(node_id: usize, dof: Dof) -> usize {
+    node_id * DOFS_PER_NODE + dof as usize
+}
