66cc96f28a4a6a1bc2bb43964ba9c12417661373
Chetan Jangir <chetanrjj2002@gmail.com>
2026-06-14 16:19:46 +0530

    feat: Configure Nix shell for wgpu runtime

--- shell.nix
+++ shell.nix
@@ -1,6 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    libglvnd
+    mesa
+    mesa-demos
+    pciutils
+    vulkan-loader
+    vulkan-tools
     wayland
     wayland-protocols
     libxkbcommon
@@ -8,8 +14,11 @@ pkgs.mkShell {
 
   shellHook = ''
     export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+      pkgs.libglvnd
+      pkgs.mesa
+      pkgs.vulkan-loader
       pkgs.wayland
       pkgs.libxkbcommon
-    ]}:$LD_LIBRARY_PATH
+    ]}:/run/opengl-driver/lib:/run/opengl-driver-32/lib:$LD_LIBRARY_PATH
   '';
 }
