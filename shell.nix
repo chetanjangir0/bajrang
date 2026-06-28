8593a85c216426ab5b5931926cd3fc9c94e897e4
Chetan Jangir <chetanrjj2002@gmail.com>
2026-06-28 11:45:58 +0530

    Revert "build(gui): add Nix Windows cross-compilation shell support"
    This reverts commit f52688e1266b71e7b16c1d1c169e3b35269b7b3b.

--- shell.nix
+++ shell.nix
@@ -1,29 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
-
-let
-  windowsTarget = "x86_64-pc-windows-gnu";
-  mingwPthreads = pkgs.pkgsCross.mingwW64.windows.pthreads.overrideAttrs (old: {
-    meta = old.meta // {
-      platforms = pkgs.lib.platforms.all;
-    };
-  });
-  linuxGuiLibraryPath = pkgs.lib.makeLibraryPath [
-    pkgs.libglvnd
-    pkgs.mesa
-    pkgs.vulkan-loader
-    pkgs.wayland
-    pkgs.libxkbcommon
-  ];
-in
 pkgs.mkShell {
-  packages = with pkgs; [
-    pkg-config
-    rustup
-
-    # Windows cross-compilation toolchain for Rust's *-pc-windows-gnu target.
-    pkgsCross.mingwW64.stdenv.cc
-    mingwPthreads
-
+  buildInputs = with pkgs; [
     libglvnd
     mesa
     mesa-demos
@@ -36,14 +13,12 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    export LD_LIBRARY_PATH=${linuxGuiLibraryPath}:/run/opengl-driver/lib:/run/opengl-driver-32/lib:$LD_LIBRARY_PATH
-
-    export WIN_CARGO_TARGET=${windowsTarget}
-    export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER="$(command -v x86_64-w64-mingw32-gcc)"
-    export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_AR="$(command -v x86_64-w64-mingw32-ar)"
-
-    echo "Windows GUI build: cargo build -p gui --release --target $WIN_CARGO_TARGET"
-    echo "If the target is missing: rustup target add $WIN_CARGO_TARGET"
-    echo "If Rustup references a deleted Nix ld-wrapper: rustup toolchain install stable --force"
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+      pkgs.libglvnd
+      pkgs.mesa
+      pkgs.vulkan-loader
+      pkgs.wayland
+      pkgs.libxkbcommon
+    ]}:/run/opengl-driver/lib:/run/opengl-driver-32/lib:$LD_LIBRARY_PATH
   '';
 }
